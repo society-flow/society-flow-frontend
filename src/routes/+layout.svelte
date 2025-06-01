030fb9f0885ca99194361cf6ef9dfeaf2c0a9ac3
4www <4www@duck.com>
2025-06-01 16:23:20 +0200

    github pages workflow and sveltekit static adapter in config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,12 @@
 <!-- src/_layout.svelte -->
-<script type="module">
+<script>
 	import { waitLocale } from 'svelte-i18n';
 	import '$lib/i18n.js';
 
 	export async function preload() {
-		// awaits for the loading of the 'en-US' and 'en' dictionaries
+		// awaits for the loading of the language ("en"...) dictionaries
 		return waitLocale();
 	}
-
-	const { children } = $props();
 </script>
 
-{@render children()}
+<slot />
