2932f819e7385f255a4a67251f5ce677e1f95db0
4www <4www@duck.com>
2025-06-07 16:17:19 +0200

    fix 404

--- src/routes/[[region]]/+error.svelte
+++ src/routes/[[region]]/+error.svelte
@@ -1,11 +1,16 @@
 <script>
 	import { page } from '$app/stores';
 	import Page from '$lib/components/routes/page.svelte';
+	import Anchor from '$lib/components/anchor.svelte';
 </script>
 
 <Page title={$page.status}>
 	{#snippet header()}
 		<h1>{$page.status}: {$page.error.message}</h1>
 	{/snippet}
-	<p>The requested page does not exist on this website.</p>
+	<p>
+		The requested page does not exist on this website. Go back to the <Anchor href="/"
+			>homepage</Anchor
+		>.
+	</p>
 </Page>
