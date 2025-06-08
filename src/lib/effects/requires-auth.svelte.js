bfc9a61f662b86b67cdd280583f1de13181e1de5
4www <4www@duck.com>
2025-06-08 10:27:14 +0200

    fix effects/requires-*auth redirects with locale

--- src/lib/effects/requires-auth.svelte.js
+++ src/lib/effects/requires-auth.svelte.js
@@ -1,8 +1,9 @@
-import { userState } from '$lib/states/user.svelte.js';
 import { base } from '$app/paths';
 import { goto } from '$app/navigation';
+import { DEFAULT_LOCALE } from '$lib/i18n.js';
+import { userState } from '$lib/states/user.svelte.js';
 
-export default function requiresAuth(locale = 'en') {
+export default function requiresAuth(locale = DEFAULT_LOCALE) {
 	const isAuth = $derived(userState.isAuth);
 	$effect(() => {
 		if (!isAuth) {
--- src/lib/effects/requires-no-auth.svelte.js
+++ src/lib/effects/requires-no-auth.svelte.js
@@ -1,10 +1,10 @@
-import { userState } from '$lib/states/user.svelte.js';
 import { base } from '$app/paths';
 import { goto } from '$app/navigation';
+import { DEFAULT_LOCALE } from '$lib/i18n.js';
+import { userState } from '$lib/states/user.svelte.js';
 
-export default function requiresNoAuth(locale = 'en') {
+export default function requiresNoAuth(locale = DEFAULT_LOCALE) {
 	const isAuth = $derived(userState.isAuth);
-
 	$effect(() => {
 		if (isAuth) {
 			setTimeout(() => goto(`${base}/${locale}/auth/logout`), 0);
