0ab2c96474cff36389add7eb074a9b48b995fc03
4www <4www@duck.com>
2025-06-08 10:52:20 +0200

    try translate NetWork error

--- src/lib/components/error.svelte
+++ src/lib/components/error.svelte
@@ -1,7 +1,10 @@
 <script lang="javascript">
+	import { _ } from 'svelte-i18n';
+
 	const { error } = $props();
+	const messageRoot = $derived(error?.message.split(' ')[0]);
 </script>
 
 {#if error?.message}
-	{error?.message}
+	{$_(`errors.${messageRoot}`, { default: error?.message })}
 {/if}
