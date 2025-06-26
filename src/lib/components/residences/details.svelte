c0cb577c133114824ff63744697a314324fea67b
4www <4www@duck.com>
2025-06-26 08:11:44 +0200

    fix residence own link

--- src/lib/components/residences/details.svelte
+++ src/lib/components/residences/details.svelte
@@ -2,7 +2,7 @@
 	import { _ } from 'svelte-i18n';
 	import Anchor from '$lib/components/anchor.svelte';
 	import RelativeDate from '$lib/components/date/relative.svelte';
-  
+
 	const { residence, society } = $props();
 </script>
 
@@ -10,7 +10,7 @@
 	<ul>
 		<li>
 			<strong>{$_('components.residences.details.name')}</strong>:
-			<Anchor href={`/societies/${residence.id}`}>{residence.residenceName}</Anchor>
+			<Anchor href={`/residences/${residence.id}`}>{residence.residenceName}</Anchor>
 		</li>
 		<li>
 			<strong>{$_('components.residences.details.societyId')}</strong>:
