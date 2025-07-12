d0b695c31b8dfcc260d37b9b1e3cb1258a03d02c
4www <4www@duck.com>
2025-07-12 22:13:01 +0200

    icon on expenses

--- src/lib/components/expenses/details.svelte
+++ src/lib/components/expenses/details.svelte
@@ -5,6 +5,13 @@
 	import Detail from '$lib/components/detail.svelte';
 	import Card from './card.svelte';
 	import SocietyCard from '$lib/components/societies/card.svelte';
+	import {
+		IconCheckboxOn,
+		IconCheckboxOff,
+		IconCalendarSelectedDate,
+		IconWatch,
+		IconCalendarEndOfMonth
+	} from 'obra-icons-svelte';
 
 	const { expense = {}, society = {} } = $props();
 </script>
@@ -14,21 +21,29 @@
 	<SocietyCard {society} />
 	<ul>
 		<li>
+			<IconCalendarEndOfMonth />
 			<strong>{$_('components.expenses.details.amountPerMonth')}:</strong>
 			{expense.amountPerMonth}
 			{society?.currency}
 		</li>
 		<li>
+			{#if expense.isActive}
+				<IconCheckboxOn />
+			{:else}
+				<IconCheckboxOff />
+			{/if}
 			<strong>{$_('components.expenses.details.active')}:</strong>
 			{expense.isActive ? 'Yes' : 'No'}
 		</li>
 		<li>
-			<strong>{$_('common.createdAt')}:</strong>
-			<RelativeDate date={expense.createdAt} />
-		</li>
-		<li>
+			<IconWatch />
 			<strong>{$_('common.updatedAt')}:</strong>
 			<RelativeDate date={expense.updatedAt} />
+		</li>
+		<li>
+			<IconCalendarSelectedDate />
+			<strong>{$_('common.createdAt')}:</strong>
+			<RelativeDate date={expense.createdAt} />
 		</li>
 	</ul>
 </Detail>
