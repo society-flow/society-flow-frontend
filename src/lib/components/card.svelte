acfe2a15b9824570428d08c7b5e9bd8dd564f18e
4www <4www@duck.com>
2025-07-15 08:36:37 +0200

    <Card show{Children,Header,Nav}=true

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,19 +1,19 @@
 <script lang="javascript">
-	const { children, header, footer } = $props();
+	const { children, header, footer, showChildren = true, showHeader = true, showFooter = true } = $props();
 </script>
 
 <article class="Card">
-	{#if header}
+	{#if header && showHeader}
 		<header class="Card-header">
 			{@render header?.()}
 		</header>
 	{/if}
-	{#if children}
+	{#if children && showChildren}
 		<main class="Card-main">
 			{@render children()}
 		</main>
 	{/if}
-	{#if footer}
+	{#if footer && showFooter}
 		<footer class="Card-footer">
 			{@render footer()}
 		</footer>
@@ -22,50 +22,50 @@
 
 <style>
 	.Card {
-    :global(.Card-header) {
-      padding: var(--s);
-    }
-    :global(.Card-main) {
-      :global(ul) {
-        padding: 0 calc(var(--s) * 4);
-        margin: 0;
-        list-style: square;
-      }
-      :global(a) {
-			  display: flex;
-			  flex-wrap: wrap;
-			  gap: var(--s);
-			  padding: calc(var(--s) * 1.5);
-			  border: 1px solid var(--c-border);
-			  border-radius: var(--border-radius);
-			  text-decoration: underline;
-			  text-decoration-color: transparent;
-			  transition: all 150ms ease-in-out;
-			  outline: 1px solid transparent;
-			  font-weight: normal;
-			  &:hover {
-				  text-decoration: underline var(--c-link);
-				  border-color: var(--c-bg2);
-				  background-color: transparent;
-				  font-weight: bold;
-			  }
-			  &:focus {
-				  outline-color: var(--c-fg);
-				  font-weight: bold;
-			  }
-			  &:active {
-				  outline-color: var(--c-bg2);
-				  border-color: var(--c-border);
-			  }
-        :global(&:has(svg)) {
-          display: flex;
-          align-items: center;
-        }
-        :global(&[aria-current="page"]) {
-          background-color: var(--c-bg2);
-          border-color: var(--c-bg);
-        }
-		  }
-    }
+		:global(.Card-header) {
+			padding: var(--s);
+		}
+		:global(.Card-main) {
+			:global(ul) {
+				padding: 0 calc(var(--s) * 4);
+				margin: 0;
+				list-style: square;
+			}
+			:global(a) {
+				display: flex;
+				flex-wrap: wrap;
+				gap: var(--s);
+				padding: calc(var(--s) * 1.5);
+				border: 1px solid var(--c-border);
+				border-radius: var(--border-radius);
+				text-decoration: underline;
+				text-decoration-color: transparent;
+				transition: all 150ms ease-in-out;
+				outline: 1px solid transparent;
+				font-weight: normal;
+				&:hover {
+					text-decoration: underline var(--c-link);
+					border-color: var(--c-bg2);
+					background-color: transparent;
+					font-weight: bold;
+				}
+				&:focus {
+					outline-color: var(--c-fg);
+					font-weight: bold;
+				}
+				&:active {
+					outline-color: var(--c-bg2);
+					border-color: var(--c-border);
+				}
+				:global(&:has(svg)) {
+					display: flex;
+					align-items: center;
+				}
+				:global(&[aria-current='page']) {
+					background-color: var(--c-bg2);
+					border-color: var(--c-bg);
+				}
+			}
+		}
 	}
 </style>
